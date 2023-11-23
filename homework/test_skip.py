10b33dcad16ad8c25c52eb046d7cef1baae0b138
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-11-23 17:22:09 +0300

    add fixtures and tests

--- homework/test_skip.py
+++ homework/test_skip.py
@@ -3,16 +3,32 @@
 Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
 """
 import pytest
+from selene import browser, have
+from selene.support.shared import browser
+
+def desktop(width):
+    return width >= 1024
 
 
-@pytest.fixture()
-def browser():
-    pass
+@pytest.fixture(params=[(1920, 1080), (1280, 960), (360, 480), (480, 800)],
+                ids=['1920 * 1080', '1280 * 960', '360 * 480', '480 * 800'])
+def browser_size(request):
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
 
 
-def test_github_desktop(browser):
-    pass
+def test_github_desktop(browser_size):
+    if browser.config.window_width < 1024:
+        pytest.skip('Test only for desktop')
+    browser.open('https://github.com/')
+    browser.element('.HeaderMenu-link--sign-in').click()
+    browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
 
 
-def test_github_mobile(browser):
-    pass
+def test_github_mobile(browser_size):
+    if browser.config.window_width >= 1024:
+        pytest.skip('Test only for mobile')
+    browser.open('https://github.com')
+    browser.element('.js-details-target.Button--link').click()
+    browser.element('a.HeaderMenu-link--sign-in').click()
+    browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
