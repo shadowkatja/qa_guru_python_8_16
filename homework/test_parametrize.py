df59c6f68fee3f4991dffa23f151cbd64037eed0
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-11-23 17:30:18 +0300

    add fixtures and tests

--- homework/test_parametrize.py
+++ homework/test_parametrize.py
@@ -3,15 +3,26 @@
 """
 import pytest
 
-
-@pytest.fixture()
-def browser():
-    pass
+from selene import browser, have
+from selene.support.shared import browser
 
 
-def test_github_desktop(browser):
-    pass
+@pytest.fixture(params=[(1280, 960), (360, 640)])
+def browser_size(request):
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
 
 
-def test_github_mobile(browser):
-    pass
+@pytest.mark.parametrize("browser_size", [(1280, 960)], ids=['1280 * 960'], indirect=True)
+def test_github_desktop(browser_size):
+    browser.open('https://github.com/')
+    browser.element('.HeaderMenu-link--sign-in').click()
+    browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
+
+
+@pytest.mark.parametrize("browser_size", [(360, 640)], ids=['360 * 640'], indirect=True)
+def test_github_mobile(browser_size):
+    browser.open('https://github.com')
+    browser.element('.js-details-target.Button--link').click()
+    browser.element('a.HeaderMenu-link--sign-in').click()
+    browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
